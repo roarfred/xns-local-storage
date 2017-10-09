80257f42cf2fd425e2e01c245043487f2b6ecab5
Roar Fredriksen <roarfred@gmail.com>
2017-10-09 23:37:34 +0200

    Accessing database

--- servelog.js
+++ servelog.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
 
+var mongodb=require('mongodb');
+
+var mongodbClient=mongodb.MongoClient;
+var mongodbURI='mongodb://localhost/sensors'
+var collection;
+
+mongodbClient.connect(mongodbURI,setupCollection);
+
+function setupCollection(err,db) {
+  if(err) throw err;
+  collection=db.collection("IoT");
+  client=mqtt.connect('mqtt://localhost')
+}
+
 app.get('/', function (req, res) {
     //res.sendFile(__dirname + '/index.htm');
     console.log('get /');
-    res.send("Hello!");
+    
+    collection.findOne({ id: "espdebugger"}, function(err, doc) {
+        res.send(doc);
+    });
+
     res.sendStatus(200);
 });
 
